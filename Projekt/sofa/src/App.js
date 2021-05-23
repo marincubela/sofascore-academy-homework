1f19d4f54bf96825b3de5440dd8b1e5eddddb9e5
Marin Ćubela <mc51493@fer.hr>
2021-05-24 00:02:39 +0200

    Add prototype for css

--- Projekt/sofa/src/App.js
+++ Projekt/sofa/src/App.js
@@ -1,8 +1,28 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Header } from "./modules/layout/Header";
+
 function App() {
   return (
-    <div className="App">
-      <header></header>
-    </div>
+    <BrowserRouter>
+      <Header></Header>
+      <Switch>
+        <Route exact path="/">
+          <h1>Start</h1>
+        </Route>
+        <Route path="/uno">
+          <h1>uno</h1>
+        </Route>
+        <Route path="/duo">
+          <h1>duo</h1>
+        </Route>
+        <Route path="/tre">
+          <h1>tre</h1>
+        </Route>
+        <Route path="*">
+          <h1>404 - page not found</h1>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
